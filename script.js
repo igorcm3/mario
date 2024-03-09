bd114d33afa1dfc2edaf8a225f0efa5f6d675083
Igor Corona de Matos <coronaigor@gmail.com>
2024-03-09 20:51:21 -0300

    feat: explosão do cogumelo e exibição do convite

--- script.js
+++ script.js
@@ -1,104 +1,125 @@
-
-
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+const mushroom = document.querySelector(".mushroom");
+const counterNumber = document.querySelector(".counter-number");
+
 const PIPE_LEFT_POSITION = 0;
 const PIPE_COLISION = 120;
 const MARIO_COLISION = 80;
-var pipeAnimationCount = 0;
+var pipeAnimationCount = 5;
 
 const jump = (event) => {
-
   if (event.code == "Space") {
     mario.classList.add("jump");
     setTimeout(() => {
-        mario.classList.remove("jump");
+      mario.classList.remove("jump");
     }, 500);
   }
 };
 
 const gameLoop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
 
-    if (pipePosition <= PIPE_COLISION && pipePosition > PIPE_LEFT_POSITION && marioPosition < MARIO_COLISION) {
-        stopAnimationsGame(pipePosition, marioPosition);
+  if (
+    pipePosition <= PIPE_COLISION &&
+    pipePosition > PIPE_LEFT_POSITION &&
+    marioPosition < MARIO_COLISION
+  ) {
+    stopAnimationsGame(pipePosition, marioPosition);
 
-        showMarioGameOver();
+    showMarioGameOver();
 
-        clearInterval(gameLoop);
+    clearInterval(gameLoop);
 
-        setTimeout(() => {
-          Swal.fire({
-            title: "Você perdeu",
-            html: `Score: <strong>${pipeAnimationCount }</strong>`,
-            confirmButtonColor: "#4C956C",
-            confirmButtonText: "Jogar novamente"
-          }).then((result) => {
-            if (result.isConfirmed) {
-              location.reload();
-            }
-          });
-      }, 100);
-    }
+    setTimeout(() => {
+      Swal.fire({
+        title: "Você perdeu",
+        html: `Pule por <strong>5</strong> obstaculos para explodir o cogumelo alucinógeno `,
+        confirmButtonColor: "#4C956C",
+        confirmButtonText: "Jogar novamente",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          location.reload();
+        }
+      });
+    }, 100);
+  }
 
-    if (pipeAnimationCount >= 1 ) {
-      stopAnimationsGame(pipePosition, marioPosition);
-      removeMarioImage();
+  if (pipeAnimationCount <= 0) {
+    stopAnimationsGame(pipePosition, marioPosition);
+    removeMarioImage();
+    animateMushroom();
+    setTimeout(() => {
+      showInvite();
+    }, 5000);
 
-      setTimeout(() => {
-        Swal.fire({
-          allowOutsideClick: false,
-          showCancelButton: false,
-          showConfirmButton: false,
-          width: 700,
-          html: `<div style="height: 655px"></div>`,
-          color: "#95D5B1",
-          background: "#fff url(images/diego-janaina.gif)",
-          showClass: {
-            popup: `
-              animate__animated
-              animate__bounceInUp
-              animate__faster
-            `
-          },
-          hideClass: {
-            popup: `
-              animate__animated
-              animate__backOutUp
-              animate__faster
-            `
-          }
-        });
-
-
-      }, 100)
-
-      clearInterval(gameLoop);
-    }
-
-}, 10)
+    clearInterval(gameLoop);
+  }
+}, 10);
 
 const stopAnimationsGame = (pipePosition, marioPosition) => {
-  pipe.style.animation = 'none';
+  pipe.style.animation = "none";
   pipe.style.left = `${pipePosition}px`;
 
-  mario.style.animation = 'none';
+  mario.style.animation = "none";
   mario.style.bottom = `${marioPosition}px`;
-}
+};
 
 const showMarioGameOver = () => {
-  mario.src = './images/game-over.png';
-  mario.style.width = '75px';
-  mario.style.marginLeft = '50px';
-}
+  mario.src = "./images/game-over.png";
+  mario.style.width = "75px";
+  mario.style.marginLeft = "50px";
+};
 
 const removeMarioImage = () => {
-  mario.style.visibility = 'hidden';
-}
+  mario.style.visibility = "hidden";
+  counterNumber.style.visibility = "hidden";
+};
+
+const animateMushroom = () => {
+  mushroom.classList.add("mushroom-animation");
+  setTimeout(() => {
+    mushroom.src = "./images/explosion.gif";
+  }, 3000);
+};
+
+const showInvite = () => {
+  Swal.fire({
+    allowOutsideClick: false,
+    showCancelButton: false,
+    showConfirmButton: false,
+    width: 700,
+    html: `<div style="height: 655px"></div>`,
+    color: "#95D5B1",
+    background: "#fff url(images/diego-janaina.gif)",
+    showClass: {
+      popup: `
+        animate__animated
+        animate__bounceInUp
+        animate__faster
+      `,
+    },
+    hideClass: {
+      popup: `
+        animate__animated
+        animate__backOutUp
+        animate__faster
+      `,
+    },
+  });
+};
 
 document.addEventListener("keydown", jump);
 
-pipe.addEventListener('animationiteration', function(event) {
-  pipeAnimationCount++;
+pipe.addEventListener("animationiteration", function (event) {
+  pipeAnimationCount--;
+
+  if ("textContent" in counterNumber) {
+    counterNumber.textContent = pipeAnimationCount;
+  } else {
+    counterNumber.innerText = pipeAnimationCount;
+  }
 });
